a30a42c1eaea63f19dfee3eb03e0b763c400a12f
zogu101 <ezeoguebuka1@gmail.com>
2025-08-20 12:32:59 +0100

    update my

--- Check_Voters/votecheck.js
+++ Check_Voters/votecheck.js
@@ -5,8 +5,12 @@ var check = document.getElementById("check-status");
 
 function checkage(){
     let numb_age = Number(age.value)
-    if(numb_age >= 18){
-        message.innerHTML = "congratulations!! "+nameEl.value+" You are eligible to vote";
+    if(nameEl.value.trim() === "" || age.value.trim() === ""){
+        message.innerHTML = "Please Enter your name and age"
+    }else if (!Number.isInteger(numb_age) || numb_age <= 0){
+        message.innerHTML = "please enter your age"
+    }else if(numb_age >= 18){
+        message.innerHTML = "Congratulations!! "+nameEl.value+" You are eligible to vote";
     }else{
         message.innerHTML = "Sorry "+nameEl.value+" you are not eligible to vote try again when you are 18yrs";
     }
